ba65d08c68e83866695c2d91106e2b4bb9ba58fb
Umut TUFAN <tufanumut@gmail.com>
2018-01-06 23:37:42 +0300

    Initial Commit

---
+++ expNetCoreSwap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace expNetCoreSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number to swap with another:");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.Write("\nEnter second number to swap with another:");
+            var y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nFirst Number:{x} Second Number:{y}");
+
+            Console.WriteLine("\nPress any key to swap numbers...");
+            Console.ReadKey();
+
+            var temp = x;
+            x = y;
+            y = temp;
+
+            Console.WriteLine($"\nFirst Number:{x} Second Number:{y}");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
